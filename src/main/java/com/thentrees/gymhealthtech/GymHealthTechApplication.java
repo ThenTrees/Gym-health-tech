3102f3528a7f6f77b9bc1389949592748fc6391e
Tran Le Thien Tri <thientri.trank17@gmail.com>
2025-08-03 19:28:59 +0700

    [feat]: init source backend.

--- src/main/java/com/thentrees/gymhealthtech/GymHealthTechApplication.java
+++ src/main/java/com/thentrees/gymhealthtech/GymHealthTechApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GymHealthTechApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GymHealthTechApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(GymHealthTechApplication.class, args);
+  }
 }
